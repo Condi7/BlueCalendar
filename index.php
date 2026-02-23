b267d32ddc0c8137303bba4fd5e0cfdea6df04ac
Condi7 <paoloconde@outlook.it>
2026-02-23 10:16:40 +0100

    v1.0.2

--- index.php
+++ index.php
@@ -321,7 +321,7 @@ header("Pragma: no-cache");
  * And away we go...
  */
 
-$GLOBALS['versionOfBlueCalendar'] = '1.0.1';
+$GLOBALS['versionOfBlueCalendar'] = '1.0.2';
 
 require_once FCPATH . 'vendor/autoload.php';
 require_once BASEPATH.'core/CodeIgniter.php';
